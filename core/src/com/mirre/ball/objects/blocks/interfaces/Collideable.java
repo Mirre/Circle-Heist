48945473edcf964f93f2d0ce4f0a60e1a680c3d2
Mirre <Mackeduki@gmail.com>
2015-02-03 22:50:16 +0100

    Moved stuff

--- core/src/com/mirre/ball/objects/blocks/interfaces/Collideable.java
+++
@@ -1,7 +0,0 @@
-package com.mirre.ball.objects.blocks.interfaces;
-
-public interface Collideable {
-	
-	
-	public boolean passThroughAble();
-}
--- core/src/com/mirre/ball/objects/blocks/interfaces/Moveable.java
+++
@@ -1,7 +0,0 @@
-package com.mirre.ball.objects.blocks.interfaces;
-
-public interface Moveable {
-
-	public abstract void update(float deltaTime);
-	
-}
